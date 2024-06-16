7e4a49873426324c544d7fbf8bd13e38617913d2
irohasuko <tottu1104@gmail.com>
2024-06-16 23:08:13 +0900

    本一覧画面の作成

---
+++ app/book/list/page.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const getAllBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/book/readAll`
+  );
+  const jsonData = await response.json();
+  const allBooks = jsonData.allBooks;
+  return allBooks;
+};
+
+export default async function ShowAllBooks() {
+  const allBooks = await getAllBooks();
+
+  return (
+    <div>
+      <h1>商品一覧</h1>
+      {allBooks.map((book) => (
+        <Link
+          href={`${process.env.NEXT_PUBLIC_API_URL}/book/detail/${book._id}`}
+          key={book._id}
+        >
+          <img src={book.img_path} width={100} height={200} />
+          <h2>{book.title}</h2>
+          <h3>{book.authors}</h3>
+          <h3>{book.isbn}</h3>
+          <p>{book.description.substring(0, 80) + "........"}</p>
+          <br />
+        </Link>
+      ))}
+    </div>
+  );
+}
