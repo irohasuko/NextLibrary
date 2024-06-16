a7adf3f5ff1202bfeedb0979c8e7e7b5da0b06f2
irohasuko <tottu1104@gmail.com>
2024-06-16 18:33:21 +0900

    本用のスキーマを作成

---
+++ app/utils/schemaModels.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  publish_year: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  img_path: {
+    type: String,
+  },
+  authors: [
+    {
+      type: String,
+    },
+  ],
+});
+
+export const BookModel =
+  mongoose.models.Book || mongoose.model("Book", BookSchema);
