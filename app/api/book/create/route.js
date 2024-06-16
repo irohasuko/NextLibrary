cdad911131fb032ffa4c186527a48297a433a09d
irohasuko <tottu1104@gmail.com>
2024-06-16 19:27:06 +0900

    本の登録処理の作成

---
+++ app/api/book/create/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/app/utils/database";
+import { BookModel } from "@/app/utils/schemaModels";
+import axios from "axios";
+
+export async function POST(request) {
+  const reqBody = await request.json();
+  const isbn = reqBody.isbn;
+
+  try {
+    // データベースと接続
+    await connectDB();
+
+    // Google_book_APIから本の情報を取得
+    const res = await axios.get(process.env.GOOGLE_BOOK_API_URL + isbn);
+    const bookAPIInfo = res.data.items[0].volumeInfo;
+
+    // APIから取ってきた情報を必要な分だけ整形
+    const bookInfo = {
+      title: bookAPIInfo.title,
+      publish_year: bookAPIInfo.publishedDate.substr(0, 4),
+      description: bookAPIInfo.description,
+      isbn: isbn,
+      img_path: bookAPIInfo.imageLinks.thumbnail,
+      authors: bookAPIInfo.authors,
+    };
+
+    // DBに登録
+    await BookModel.create(bookInfo);
+
+    return NextResponse.json({ message: "本の登録に成功", bookInfo });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ message: "本の登録に失敗" });
+  }
+}
