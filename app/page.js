74ff98aea1bc94242deb3ddcfc0840bb4e2cee42
irohasuko <tottu1104@gmail.com>
2024-06-19 23:26:54 +0900

    ホーム画面のスタイリング

--- app/page.js
+++ app/page.js
@@ -2,10 +2,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/book/create">本の追加</Link>
-      <Link href="/book/list">本の一覧</Link>
-      <Link href="/book/search">本の検索</Link>
-    </main>
+    <Main>
+      <div class="w-2/3 p-4">
+        <Link href="/lending/lend">
+          <Card>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+              蔵書管理
+            </h2>
+            <p class="leading-relaxed text-base">
+              蔵書の追加や一覧表示/検索が行えます
+            </p>
+          </Card>
+        </Link>
+        <Link href="/lending/return">
+          <Card>
+            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+              貸出管理
+            </h2>
+            <p class="leading-relaxed text-base">
+              蔵書の貸出/返却処理が行えます
+            </p>
+          </Card>
+        </Link>
+      </div>
+    </Main>
   );
 }
