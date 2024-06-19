20d5189dee3fbe4ed6358fb2298ee88c6cc0e00e
irohasuko <tottu1104@gmail.com>
2024-06-19 23:05:46 +0900

    一覧画面のスタイリング

--- app/book/list/page.js
+++ app/book/list/page.js
@@ -1,3 +1,5 @@
+import Main from "@/app/components/Main";
+import PageTitle from "@/app/components/PageTitle";
 import Link from "next/link";
 
 const getAllBooks = async () => {
@@ -16,22 +18,22 @@ export default async function ShowAllBooks() {
   const allBooks = await getAllBooks();
 
   return (
-    <div>
-      <h1>蔵書一覧</h1>
+    <Main>
+      <PageTitle title="蔵書一覧" />
       {allBooks.map((book) => (
         <Link
           href={`${process.env.NEXT_PUBLIC_API_URL}/book/detail/${book._id}`}
           key={book._id}
+          class="flex items-center mb-4 w-2/3 border border-gray-200 bg-white p-6 rounded-lg "
         >
-          <img src={book.img_path} width={100} height={200} />
-          <h2>{book.title}</h2>
-          <h3>{book.authors}</h3>
-          <h3>{book.isbn}</h3>
-          <p>{book.description.substring(0, 20)}</p>
-          <p>{book.is_lend ? "貸し出し中" : "貸し出し可"}</p>
-          <br />
+          <img class="mr-5" src={book.img_path} width={100} height={200} />
+          <div class="text-left">
+            <h3 class="font-bold">{book.title}</h3>
+            <p>著者：{book.authors.join("/")}</p>
+            <p>{book.is_lend ? "貸し出し中" : "貸し出し可"}</p>
+          </div>
         </Link>
       ))}
-    </div>
+    </Main>
   );
 }
