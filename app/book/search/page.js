8afdbd572d5e7680a6921ef02832146e18af134b
irohasuko <tottu1104@gmail.com>
2024-06-19 23:09:35 +0900

    検索ページのスタイリング

--- app/book/search/page.js
+++ app/book/search/page.js
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import Main from "@/app/components/Main";
+import PageTitle from "@/app/components/PageTitle";
 
 const SearchBooks = () => {
   const [word, setWord] = useState("");
@@ -31,30 +33,31 @@ const SearchBooks = () => {
   };
 
   return (
-    <div>
-      <h1>本情報の検索</h1>
+    <Main>
+      <PageTitle title="蔵書検索" />
       <input
+        class="block w-2/3 flex-1 py-1.5 pl-1 mb-10 border rounded text-gray-900 placeholder:text-gray-400 focus:ring-0"
         value={word}
         onChange={handleChange}
         type="text"
         name="isbn"
         placeholder="タイトル、著者名、説明で検索"
-        required
       />
       {searchedBooks.map((book) => (
         <Link
           href={`${process.env.NEXT_PUBLIC_API_URL}/book/detail/${book._id}`}
           key={book._id}
+          class="flex items-center mb-4 w-2/3 border border-gray-200 bg-white p-6 rounded-lg "
         >
-          <img src={book.img_path} width={100} height={200} />
-          <h2>{book.title}</h2>
-          <h3>{book.authors}</h3>
-          <h3>{book.isbn}</h3>
-          <p>{book.description.substring(0, 80) + "........"}</p>
-          <br />
+          <img class="mr-5" src={book.img_path} width={100} height={200} />
+          <div class="text-left">
+            <h3 class="font-bold">{book.title}</h3>
+            <p>著者：{book.authors.join("/")}</p>
+            <p>{book.is_lend ? "貸し出し中" : "貸し出し可"}</p>
+          </div>
         </Link>
       ))}
-    </div>
+    </Main>
   );
 };
 
