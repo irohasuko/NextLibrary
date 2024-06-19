bcf41e0ac87acf290a4bb43276db4fcb40bbbb9d
irohasuko <tottu1104@gmail.com>
2024-06-19 22:02:51 +0900

    ボタン用コンポーネントの作成

---
+++ app/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ children }) => {
+  return (
+    <button class="flex mx-auto mt-5 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
