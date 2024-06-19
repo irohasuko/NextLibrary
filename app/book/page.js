8a45560519b168509070fb5c09b8448a207be043
irohasuko <tottu1104@gmail.com>
2024-06-19 21:32:53 +0900

    蔵書管理ページのスタイリング

--- app/book/page.js
+++ app/book/page.js
@@ -1,11 +1,46 @@
 import Link from "next/link";
+import PageTitle from "../components/PageTitle";
+import Main from "../components/Main";
+import Card from "../components/Card";
 
 export default function Book() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/book/create">本の追加</Link>
-      <Link href="/book/list">本の一覧</Link>
-      <Link href="/book/search">本の検索</Link>
-    </main>
+    <>
+      <PageTitle title="蔵書管理" />
+      <Main>
+        <div class="w-2/3 p-4">
+          <Link href="/book/create">
+            <Card>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                蔵書の追加
+              </h2>
+              <p class="leading-relaxed text-base">
+                ISBN番号や書籍のバーコードから蔵書を追加します
+              </p>
+            </Card>
+          </Link>
+          <Link href="/book/list">
+            <Card>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                蔵書の一覧
+              </h2>
+              <p class="leading-relaxed text-base">
+                現在存在する蔵書の一覧が確認できます
+              </p>
+            </Card>
+          </Link>
+          <Link href="/book/search">
+            <Card>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                蔵書の検索
+              </h2>
+              <p class="leading-relaxed text-base">
+                現在存在する蔵書を検索できます
+              </p>
+            </Card>
+          </Link>
+        </div>
+      </Main>
+    </>
   );
 }
