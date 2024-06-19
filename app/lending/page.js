57f7c99cb76a30bb72d0c74c1c0bf3872ef7cb25
irohasuko <tottu1104@gmail.com>
2024-06-19 21:48:40 +0900

    蔵書の貸出管理ページを追加＆スタイリング

---
+++ app/lending/page.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import PageTitle from "../components/PageTitle";
+import Main from "../components/Main";
+import Card from "../components/Card";
+
+export default function Book() {
+  return (
+    <>
+      <PageTitle title="貸出管理" />
+      <Main>
+        <div class="w-2/3 p-4">
+          <Link href="/lending/lend">
+            <Card>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                蔵書の貸し出し
+              </h2>
+              <p class="leading-relaxed text-base">
+                蔵書の貸し出し処理を行います
+              </p>
+            </Card>
+          </Link>
+          <Link href="/lending/return">
+            <Card>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                蔵書の返却
+              </h2>
+              <p class="leading-relaxed text-base">蔵書の返却処理を行います</p>
+            </Card>
+          </Link>
+        </div>
+      </Main>
+    </>
+  );
+}
